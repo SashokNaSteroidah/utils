1ca2cd0fc60858d3e156ef341febf5a3eabf2195
merch2 <merch2@bwg-io.site>
2024-02-11 01:43:53 +0300

    dev: change consts for page

--- src/feature/mainPage/consts/mainPage.consts.ts
+++ src/feature/mainPage/consts/mainPage.consts.ts
@@ -1,7 +1,14 @@
 export const MainCards = [
     {
+        id: "1",
         name: "Генератор",
         subName: "UUID",
         link: "/uuid",
-    }
+    },
+    {
+        id: "2",
+        name: "Декодировние",
+        subName: "из Base64",
+        link: "/decode-base64",
+    },
 ]
